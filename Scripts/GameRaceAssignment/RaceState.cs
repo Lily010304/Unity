5f857e9f8f5efa4947ed7b061dadfb90d6e7330b
Layla <l.m.alsaabna@students.ptuk.edu.ps>
2024-12-18 09:54:17 +0200

    Game State Assignmrnt Scripts

---
+++ Scripts/GameRaceAssignment/RaceState.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RaceGame
+{
+public enum RaceState
+{
+    Start,
+    Accelerate,
+    Turn,
+    Crash,
+    Finish
+
+}
+}
